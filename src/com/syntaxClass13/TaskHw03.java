f0482572a98b026aad023f6309c7374ef7919bf7
sebastian <sebassg8809@gmail.com>
2021-11-08 21:11:20 -0500

    new

---
+++ src/com/syntaxClass13/TaskHw03.java
@@ -0,0 +1,16 @@
+package com.syntaxClass13;
+
+public class TaskHw03 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String name="ABCD";
+		StringBuffer sb=new StringBuffer(name);
+		
+		System.out.println(sb.reverse());
+		
+		
+
+	}
+
+}
